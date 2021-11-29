8830c8412ac608f37e0899e96bf7b0d4e13883f9
louisonc <cephise.louison@cea.fr>
2021-11-29 19:08:57 +0100

    add type of content

--- src/app/day/day.component.ts
+++ src/app/day/day.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import DataBase from '../../assets/data/day-data.json';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
-interface UserData {
+  interface UserData {
   user: string;
   contents: Day[];
 }
 interface Day {
   day: number;
-  message: string;
+  data: Element[];
+}
+interface Element {
+  type: string;
+  content: string;
 }
 
 @Component({
@@ -23,7 +28,7 @@ export class DayComponent implements OnInit {
   data: UserData | undefined = undefined;
   idData = -1;
   dataFound = false;
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private sanitizer: DomSanitizer) {
    }
 
   ngOnInit(): void {
@@ -39,5 +44,9 @@ export class DayComponent implements OnInit {
     if(this.data)
     {this.idData = this.data?.contents.findIndex( d => d.day === this.day); this.dataFound=true;}
   }
+
+  videoURL(temp: string) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube-nocookie.com/embed/${temp}?controls=0`);
+ }
 
 }
