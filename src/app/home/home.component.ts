d97e7071c66fdc87b9f9f62bb8142d96667a883f
louisonc <cephise.louison@cea.fr>
2021-12-03 09:09:01 +0100

    add envelope in home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,8 @@ export class HomeComponent implements OnInit {
   counter = counter;
   numberOfDay = 24;
   freeDay = 0;
+  envPath = "assets/pictures/envelopes/"
+
   constructor() { }
 
   ngOnInit(): void {
